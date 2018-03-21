b5c479bbb8dec6a3b00023ca299111cc581ac180
Chris Keckler, Savio <keckler@berkeley.edu>
2018-03-21 15:05:08 -0700

    removed old ADOPT runs, which had numerous errors and did not converge to criticality

--- ADOPT/x_sysgeom.py
+++
@@ -1,9 +0,0 @@
-def systemgeometry(LowerCoolantPlenaLength, BelowCoreChannelLength, FuelLength, AboveCoreChannelLength,
-				   IHXLength, IHXToTop, ThermalCenterElevation, InternalRadialComponentWidth, CoreDiameter,
-				   SystemDiameter, PrimaryVesselThickness):
-
-	CoreAssemblyLength  = BelowCoreChannelLength + FuelLength + AboveCoreChannelLength
-	TotalSystemDiameter = SystemDiameter + InternalRadialComponentWidth*2 + PrimaryVesselThickness*2
-	TotalSystemHeight   = LowerCoolantPlenaLength + CoreAssemblyLength + ThermalCenterElevation + (IHXLength/2) + IHXToTop
-	
-	return(TotalSystemHeight, TotalSystemDiameter)
