1127aef3c315eb7eae088eea87ab592f044fb0b1
Chris Keckler <keckler@berkeley.edu>
2018-03-30 15:17:24 -0700

    added details for all of the layouts to the optimization file

--- layoutOpt/layoutOpt.m
+++ layoutOpt/layoutOpt.m
@@ -1,10 +1,34 @@
 clear all;
 
+% % for A
+% validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, [4:7, 11:15, 20:23, 27:31, 36:39, 43:47]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
+% pitch = 22.05; %cm
+% batchRadii = [0.0, (53.065+75.045)/2, (75.045+91.911)/2, (91.911+106.129)/2, (106.129+118.656)/2, (118.656+129.981)/2, (129.981+140.396)/2];
+
+% for A2
 validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, [4:7, 11:15, 20:23, 27:31, 36:39, 43:47]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
 pitch = 22.05; %cm
-batchRadii = [0.0, (53.065+75.045)/2, (75.045+91.911)/2, (91.911+106.129)/2, (106.129+118.656)/2, (118.656+129.981)/2, (129.981+140.396)/2];
-% for C
-%batchRadii = [0.0, (119.5+84.5)/2, (146.3+119.5)/2, (168.9+146.3)/2, (188.9+168.9)/2, (206.9+188.9)/2, (223.5+206.9)/2]; %cm
+batchRadii = [0.0, (37.523+53.065)/2, (53.065+64.991)/2, (64.991+75.045)/2, (75.045+83.903)/2, (83.903+91.911)/2, (91.911+99.275)/2, (99.275+106.129)/2, (106.129+112.567)/2, (112.567+118.656)/2, (118.656+124.447)/2, (124.447+129.981)/2, (129.981+135.289)/2, (135.289+140.396)/2];
+
+% % for B
+% pitch = 22.05; %cm
+% validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, 1:48, 1:54, [4:8, 14:18, 24:28, 34:38, 44:48, 54:58]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
+% batchRadii = [0.0, (65.760+92.998)/2, (92.998+113.899)/2, (113.899+131.519)/2, (131.519+147.043)/2, (147.043+161.078)/2, (161.078+173.984)/2]; %cm
+
+% % for C
+% pitch = 22.05; %cm
+% validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, 1:48, 1:54, 1:60, 1:66, [2:12, 14:24, 26:36, 38:48, 50:60, 62:72], [5:9, 18:23, 31:36, 44:48, 57:62, 70:75]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
+% batchRadii = [0.0, (119.5+84.5)/2, (146.3+119.5)/2, (168.9+146.3)/2, (188.9+168.9)/2, (206.9+188.9)/2, (223.5+206.9)/2]; %cm
+
+% % for D1
+% pitch = 22.05; %cm
+% validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, 1:48, 1:54, 1:60, 1:66, 1:72, 1:78, [3:13, 17:27, 31:41, 45:55, 59:69, 73:83], [7:10, 22:25, 37:40, 52:55, 67:70, 82:85]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
+% batchRadii = [0.0, (95.664+135.289)/2, (135.289+165.694)/2, (165.694+191.327)/2, (191.327+213.910)/2, (213.910+234.326)/2, (234.326+253.101)/2]; %cm
+
+% % for D2
+% pitch = 22.05; %cm
+% validPositions = {[1], 1:6, 1:12, 1:18, 1:24, 1:30, 1:36, 1:42, 1:48, 1:54, 1:60, 1:66, 1:72, 1:78, [3:13, 17:27, 31:41, 45:55, 59:69, 73:83], [7:10, 22:25, 37:40, 52:55, 67:70, 82:85]}; %cell containing a vector for each ring. each vector contians the position numbers which are valid in each ring for assembly placement
+% batchRadii = [0.0, (70.198+99.275)/2, (99.275+121.586)/2, (121.586+140.396)/2, (140.396+156.967)/2, (156.967+171.949)/2, (171.949+185.726)/2, (185.726+198.549)/2, (198.549+210.593)/2, (210.593+221.985)/2, (221.985+232.819)/2, (232.819+243.172)/2, (243.172+253.101)/2]; %cm
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % begin code
@@ -122,7 +146,6 @@ for batch = 1:numBatches
                 Aineq((batch-1)*12+12, (batch-1)*numAss+ass) = -1;
             else
                 disp('error: something is wrong with the symmetry partitioning');
-                disp(pos);
             end
         end
     end
